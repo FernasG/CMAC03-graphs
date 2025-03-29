3c57463b21ef192ee654475fc83e2b263853c931
FernasG <fernandogoul7@gmail.com>
2025-03-28 22:24:56 -0300

    refactor: update output function to save file with content requested

--- src/file_manager.py
+++ src/file_manager.py
@@ -6,9 +6,10 @@ Read file from datasets folder and create a Numpy Array with file content
 """
 def convert_file_to_np_array(filename: str):
     filepath = path.join("datasets", filename)
-    f = open(filepath, "rb")
 
+    f = open(filepath, "rb")
     data = np.genfromtxt(f, dtype="int32")
+    f.close()
 
     return data
 
@@ -17,8 +18,13 @@ def convert_file_to_np_array(filename: str):
 Save matrix content into a file and show infos about the matrix
 """
 def save_np_array_to_file(instance: str, matrix: np.ndarray, filename: str = "result.txt"):
+    filepath = path.join("src", filename)
     nrows, ncols = matrix.shape
-    print(f"{instance} {nrows} {ncols}")
 
-    np.savetxt(filename, matrix, fmt="%d", delimiter=" ")
+    result = f"{instance} {nrows} {ncols}"
+
+    with open(filepath, "w") as f:
+        f.write(result)
+
+    print(result)
 
